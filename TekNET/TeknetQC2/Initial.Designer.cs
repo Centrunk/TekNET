774ebcf9f88cb6dc036663ca6613b318c0f3e59d
Nyx <hello@rosesam.pw>
2025-06-07 15:17:36 -0500

    Very important update.... :3

--- TekNET/TeknetQC2/Initial.Designer.cs
+++ TekNET/TeknetQC2/Initial.Designer.cs
@@ -28,12 +28,79 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Initial";
+			savebut = new Button();
+			comboBox1 = new ComboBox();
+			label1 = new Label();
+			label2 = new Label();
+			textBox1 = new TextBox();
+			SuspendLayout();
+			// 
+			// savebut
+			// 
+			savebut.Location = new Point(653, 146);
+			savebut.Name = "savebut";
+			savebut.Size = new Size(75, 23);
+			savebut.TabIndex = 0;
+			savebut.Text = "Save";
+			savebut.UseVisualStyleBackColor = true;
+			// 
+			// comboBox1
+			// 
+			comboBox1.FormattingEnabled = true;
+			comboBox1.Location = new Point(632, 48);
+			comboBox1.Name = "comboBox1";
+			comboBox1.Size = new Size(121, 23);
+			comboBox1.TabIndex = 1;
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new Point(669, 31);
+			label1.Name = "label1";
+			label1.Size = new Size(38, 15);
+			label1.TabIndex = 2;
+			label1.Text = "label1";
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.Location = new Point(673, 79);
+			label2.Name = "label2";
+			label2.Size = new Size(38, 15);
+			label2.TabIndex = 3;
+			label2.Text = "label2";
+			// 
+			// textBox1
+			// 
+			textBox1.Location = new Point(632, 97);
+			textBox1.Name = "textBox1";
+			textBox1.Size = new Size(121, 23);
+			textBox1.TabIndex = 4;
+			// 
+			// Initial
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			BackgroundImage = Properties.Resources.IMG_1451;
+			BackgroundImageLayout = ImageLayout.Center;
+			ClientSize = new Size(800, 450);
+			Controls.Add(textBox1);
+			Controls.Add(label2);
+			Controls.Add(label1);
+			Controls.Add(comboBox1);
+			Controls.Add(savebut);
+			Name = "Initial";
+			Text = "Initial";
+			ResumeLayout(false);
+			PerformLayout();
 		}
 
 		#endregion
+
+		private Button savebut;
+		private ComboBox comboBox1;
+		private Label label1;
+		private Label label2;
+		private TextBox textBox1;
 	}
 }
