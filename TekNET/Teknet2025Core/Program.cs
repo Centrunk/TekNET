c713d615cce22b0ee1328c6aca7ecb6987b64da0
Nyx <hello@rosesam.pw>
2025-06-07 15:12:28 -0500

    Fix Whoops

--- TekNET/Teknet2025Core/Program.cs
+++ TekNET/Teknet2025Core/Program.cs
@@ -11,7 +11,7 @@ namespace Teknet2025Core
 			// To customize application configuration such as set high DPI settings or default font,
 			// see https://aka.ms/applicationconfiguration.
 			ApplicationConfiguration.Initialize();
-			Application.Run(new MainForm());
+			Application.Run(new SvrHome());
 		}
 	}
 }
